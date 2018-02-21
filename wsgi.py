0ab044a3eafa8872f5addd44ec2199ed791ede5b
jokuttan <joeltabraham@gmail.com>
2018-02-21 05:12:35 +0000

    added by jo

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    return ["<h1 style='color:blue'>Hello joelta!</h1>"]
+    return ["<h1 style='color:blue'>Hello joel!</h1>"]
 print('Hello, joel!')
