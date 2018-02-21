e37c10f0a117143a1c3143bc3552101d8f9be75e
jokuttan <joeltabraham@gmail.com>
2018-02-21 04:47:05 +0000

    changed name

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    return ["<h1 style='color:blue'>Hello There!</h1>"]
+    return ["<h1 style='color:blue'>Hello joelta!</h1>"]
 print('Hello, joel!')
