ee139916c3cd41af9034cb49438f0dddcbadf964
jokuttan <joeltabraham@gmail.com>
2018-02-21 04:35:11 +0000

    Hello

---
+++ wsgi.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return ["<h1 style='color:blue'>Hello There!</h1>"]
+print('Hello, joel!')
